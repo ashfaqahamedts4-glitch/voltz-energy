40aa6c9b2d1659b85ae3b944b3b9324ddf9ad7dc
Ashfaq Ahamed <ashfaqahamed.ASJTech@almatar.com>
2026-06-03 23:48:55 +0530

    Initial commit

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { HeroComponent } from '../../components/hero/hero.component';
+import { AboutComponent } from '../../components/about/about.component';
+import { ServicesComponent } from '../../components/services/services.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    NavbarComponent,
+    HeroComponent,
+    AboutComponent,
+    ServicesComponent,
+    FooterComponent
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
